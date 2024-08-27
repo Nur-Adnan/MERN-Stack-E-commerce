b2f376ec11bb81d90440f327f07d36cb4de188ac
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-08-27 14:30:58 +0600

    firebase added completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Suspense, lazy} from "react";
 import Loader from "./components/loader";
 import Header from "./components/header";
+import { Toaster } from "react-hot-toast";
+
+
 const Home = lazy(() => import("./pages/home"));
 const Search = lazy(() => import("./pages/search"));
 const Cart = lazy(() => import("./pages/cart"));
@@ -78,6 +81,7 @@ const App = () => {
                     </Route>;
             </Routes>
         </Suspense>
+        <Toaster position="bottom-center" />
     </Router>
   )
 }
