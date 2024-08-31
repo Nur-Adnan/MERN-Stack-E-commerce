5113c55744575026a675f4248544a9043fc94183
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-08-31 16:30:02 +0600

    search component updated

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,20 +1,67 @@
 import { useState } from "react";
 import ProductCard from "../components/product-card";
+import {
+  useCategoriesQuery,
+  useSearchProductsQuery,
+} from "../redux/api/productAPI";
+import { CustomError } from "../types/api-types";
+import toast from "react-hot-toast";
+import { Skeleton } from "../components/loader";
+import { CartItem } from "../types/types";
+import { addToCart } from "../redux/reducer/cartReducer";
+import { useDispatch } from "react-redux";
+import { useSearchParams } from "react-router-dom";
 
 const Search = () => {
- const [search, setSearch] = useState("");
- const [sort, setSort] = useState("");
- const [maxPrice, setMaxPrice] = useState(100000);
- const [category, setCategory] = useState("");
- const [page, setPage] = useState(1);
- const addToCartHandler = () => {
+  const searchQuery = useSearchParams()[0];
 
- }
- const isPrevPage = page > 1;
- const isNextPage = page < 4;
+  const {
+    data: categoriesResponse,
+    isLoading: loadingCategories,
+    isError,
+    error,
+  } = useCategoriesQuery("");
+
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [maxPrice, setMaxPrice] = useState(100000);
+  const [category, setCategory] = useState(searchQuery.get("category") || "");
+  const [page, setPage] = useState(1);
+  const {
+    isLoading: productLoading,
+    data: searchedData,
+    isError: productIsError,
+    error: productError,
+  } = useSearchProductsQuery({
+    search,
+    sort,
+    category,
+    page,
+    price: maxPrice,
+  });
+
+  const dispatch = useDispatch();
+
+  const addToCartHandler = (cartItem: CartItem) => {
+    if (cartItem.stock < 1) return toast.error("Out of Stock");
+    dispatch(addToCart(cartItem));
+    toast.success("Added to cart");
+  };
+  const isPrevPage = page > 1;
+  const isNextPage = page < 4;
+
+  if (isError) {
+    const err = error as CustomError;
+    toast.error(err.data.message);
+  }
+  if (productIsError) {
+    const err = productError as CustomError;
+    toast.error(err.data.message);
+  }
+
   return (
     <div className="product-search-page">
-        <aside>
+      <aside>
         <h2>Filters</h2>
         <div>
           <h4>Sort</h4>
@@ -43,8 +90,12 @@ const Search = () => {
             onChange={(e) => setCategory(e.target.value)}
           >
             <option value="">ALL</option>
-            <option value="">Sample1</option>
-            <option value="">Sample2</option>
+            {!loadingCategories &&
+              categoriesResponse?.categories.map((i) => (
+                <option key={i} value={i}>
+                  {i.toUpperCase()}
+                </option>
+              ))}
           </select>
         </div>
       </aside>
@@ -56,11 +107,24 @@ const Search = () => {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
+        {productLoading ? (
+          <Skeleton length={10} />
+        ) : (
           <div className="search-product-list">
-          <ProductCard productId="adsada" name="Macbook" price={4545} stock={435} handler={addToCartHandler}
-                photos="https://m.media-amazon.com/images/I/31ilq3hPhEL.jpg"
-            />
+            {searchedData?.products.map((i) => (
+              <ProductCard
+                key={i._id}
+                productId={i._id}
+                name={i.name}
+                price={i.price}
+                stock={i.stock}
+                handler={addToCartHandler}
+                photos={i.photos}
+              />
+            ))}
           </div>
+        )}
+        {searchedData && searchedData.totalPage > 1 && (
           <article>
             <button
               disabled={!isPrevPage}
@@ -69,7 +133,7 @@ const Search = () => {
               Prev
             </button>
             <span>
-              {page} of {4}
+              {page} of {searchedData.totalPage}
             </span>
             <button
               disabled={!isNextPage}
@@ -78,9 +142,10 @@ const Search = () => {
               Next
             </button>
           </article>
+        )}
       </main>
     </div>
-  )
-}
+  );
+};
 
 export default Search;
