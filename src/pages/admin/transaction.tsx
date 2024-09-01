5762944a9c4efefb70741192b8f91fa71936087e
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-09-02 00:52:13 +0600

    transaction page updated

--- src/pages/admin/transaction.tsx
+++ src/pages/admin/transaction.tsx
@@ -1,14 +1,14 @@
 import { ReactElement, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Column } from "react-table";
 import AdminSidebar from "../../components/admin/AdminSidebar";
 import TableHOC from "../../components/admin/TableHOC";
-import { useSelector } from "react-redux";
-import { RootState } from "@reduxjs/toolkit/query";
-import { useAllOrdersQuery } from "../../redux/api/orderAPI";
-import { CustomError } from "../../types/api-types";
-import toast from "react-hot-toast";
 import { Skeleton } from "../../components/loader";
+import { useAllOrdersQuery } from "../../redux/api/orderAPI";
+import { RootState } from "../../redux/store";
+import { CustomError } from "../../types/api-types";
 
 interface DataType {
   user: string;
